32c7cd5756f25536a747e41df2735882a3097b8b
David Villacis <david.villacis02|epn.edu.ec>
2019-10-31 17:02:53 -0500

    added change in operator

--- src/operators/ScalarROFAdjointOperator.jl
+++ src/operators/ScalarROFAdjointOperator.jl
@@ -7,37 +7,36 @@ struct ScalarROFAdjointOperator{T,M<:Float64} <: LinearOperator
 	u::AbstractArray{M}
     K::Variation
     Ku::AbstractArray{M}
-    nKu::AbstractArray{M}
+	nKu::AbstractArray{M}
+	lambda::Float64
+	n::Integer
+	m::Integer
 end
 
 # Constructor
 ###standard constructor Operator{N}(DomainType::Type, DomainDim::NTuple{N,Int})
-function ScalarROFAdjointOperator(DomainType::Type, u::AbstractArray{M}, K::Variation, Ku::AbstractArray{M},nKu::AbstractArray{M}) where {N,M}
-	ScalarROFAdjointOperator{DomainType,M}(u,K,Ku,nKu)
+function ScalarROFAdjointOperator(DomainType::Type, u::AbstractArray{M}, K::Variation, Ku::AbstractArray{M},nKu::AbstractArray{M},lambda::Float64,n::Integer,m::Integer) where {N,M}
+	ScalarROFAdjointOperator{DomainType,M}(u,K,Ku,nKu,lambda,n,m)
 end
 ###
 
-function ScalarROFAdjointOperator(u::AbstractArray{M}) where {M}
+function ScalarROFAdjointOperator(u::AbstractArray{M},lambda::Float64) where {M}
 	K = Variation(size(u))
 	Ku = K*u
 	nKu = sqrt.(sum(Ku.^2, dims=2))
-    ScalarROFAdjointOperator(Float64,u,K,Ku,nKu)
+	(n,m) = size(u)
+    ScalarROFAdjointOperator(Float64,u,K,Ku,nKu,lambda,n,m)
 end
 
 # Mappings
 function mul!(y::AbstractArray{T,N}, L::ScalarROFAdjointOperator{T,M}, b::AbstractArray{T,D}) where {T,N,M,D}
-	Kp = L.K*b
-	a1 = Kp./L.nKu
-	a2 = Kp*L.Ku'
-	#a3 = a2./(L.nKu.^3)
-	# a4 = L.Ku.*vcat(a3...)
-	y = a1-a2
+	y = b[1] - L.lambda * L.K' * b[2]
 end
 
 # Properties
 domainType(L::ScalarROFAdjointOperator{T, M}) where {T, M} = T
 codomainType(L::ScalarROFAdjointOperator{T, M}) where {T, M} = T
 
-size(L::ScalarROFAdjointOperator) = (size(L.Ku),size(L.u))
+size(L::ScalarROFAdjointOperator) = ((3*L.m*L.n,1),(3*L.m*L.n,1))
 
 fun_name(L::ScalarROFAdjointOperator)  = "H"
