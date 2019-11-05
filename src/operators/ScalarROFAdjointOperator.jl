fa94090a1a8b655a7aeb987229421d33c54e1025
David Villacis <david.villacis02|epn.edu.ec>
2019-11-05 14:12:08 -0500

    Fixed adjoint operator

--- src/operators/ScalarROFAdjointOperator.jl
+++ src/operators/ScalarROFAdjointOperator.jl
@@ -30,13 +30,29 @@ end
 
 # Mappings
 function mul!(y::AbstractArray{T,N}, L::ScalarROFAdjointOperator{T,M}, b::AbstractArray{T,D}) where {T,N,M,D}
-	y = b[1] - L.lambda * L.K' * b[2]
+	s = L.m*L.n
+	p = reshape(b[1:s],(L.m,L.n))
+	Kp = L.K * p
+	nu = reshape(b[s+1:end],(s,2))
+	t = p - L.lambda * L.K' * nu
+	v = zeros(s,2)
+	w = zeros(s,2)
+	for j = 1:s
+		if L.nKu[j] > 1e-3
+			v[j,:] = nu[j,:] - Kp[j,:]./L.nKu[j]
+			w[j,:] = Kp[j,:]
+		else
+			v[j,:] = zeros(1,2)
+			w[j,:] = zeros(1,2)
+		end
+	end
+	y .= [reshape(t,(s,1));reshape(v,(2*s,1));reshape(w,(2*s,1))]
 end
 
 # Properties
 domainType(L::ScalarROFAdjointOperator{T, M}) where {T, M} = T
 codomainType(L::ScalarROFAdjointOperator{T, M}) where {T, M} = T
 
-size(L::ScalarROFAdjointOperator) = ((3*L.m*L.n,1),(3*L.m*L.n,1))
+size(L::ScalarROFAdjointOperator) = ((5*L.m*L.n,1),(3*L.m*L.n,1))
 
 fun_name(L::ScalarROFAdjointOperator)  = "H"
